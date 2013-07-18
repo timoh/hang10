fe0069b4aa9cbbc522911a75521bfbac1ce76bbc
Timo H <timo.herttua@aalto.fi>
2013-07-19 02:32:21 +0300

    landing page v3

--- web.js
+++ web.js
@@ -3,10 +3,16 @@ var fs = require('fs');
 
 var app = express.createServer(express.logger());
 
+app.configure(function(){
+  app.use(express.static(__dirname + '/public'));
+});
+
+
 app.get('/', function(request, response) {
   var buffer = new Buffer(fs.readFileSync('index.html'));
   var index_html = buffer.toString();
   response.send(index_html);
+  res.write(fs.readFileSync(__dirname + path, 'utf8'));
 });
 
 var port = process.env.PORT || 8080;
