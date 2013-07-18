9a86cb35760363250ad19535fc24dc3d75a235b5
Timo H <timo.herttua@aalto.fi>
2013-07-18 16:46:30 +0300

    read from fs

--- web.js
+++ web.js
@@ -1,9 +1,12 @@
 var express = require('express');
+var fs = require('fs');
 
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World! 2');
+  var buffer = new Buffer(fs.readFileSync('index.html'));
+  var index_html = buffer.toString();
+  response.send(index_html);
 });
 
 var port = process.env.PORT || 5000;
